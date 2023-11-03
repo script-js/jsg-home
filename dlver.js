84cc5ec6d55a28c3ce5c96b80acec49519a54478
script-js <134544514+script-js@users.noreply.github.com>
2023-11-03 09:55:58 -0700

    Create dlver.js

---
+++ dlver.js
@@ -0,0 +1,8 @@
+var onlinever = "1";
+
+if (onlinever != dlver) {
+  var con1 = confirm("There is a new verion avalible. Would you like to download it?")
+  if (con1 == true) {
+    location.replace("https://script-js.github.io/jsg-home/jsg-download.zip")
+  }
+}
