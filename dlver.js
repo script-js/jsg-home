97e5e45f7ccc6b113b587d5e079a80bf9b6eb336
script-js <134544514+script-js@users.noreply.github.com>
2023-12-12 12:13:54 -0800

    Update dlver.js

--- dlver.js
+++ dlver.js
@@ -1,8 +1,1 @@
-var onlinever = "3";
-
-if (onlinever != dlver) {
-  var con1 = confirm("There is a new verion avalible. Would you like to download it?")
-  if (con1 == true) {
-    location.replace("https://script-js.github.io/jsg-home/jsg-download.zip")
-  }
-}
+alert("Download is no longer supported")
